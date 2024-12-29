472401144dc9221ef65db8dd06fa5e06e1980a91
Meghan Denny <hello@nektro.net>
2024-12-28 19:22:27 -0800

    update to zig 0.13

--- build.zig
+++ build.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const deps = @import("./deps.zig");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
 
     const exe = b.addExecutable(.{
         .name = "zig-oauth2",
-        .root_source_file = .{ .path = "./main.zig" },
+        .root_source_file = b.path("main.zig"),
         .target = target,
         .optimize = mode,
     });
