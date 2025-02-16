5f25e67a03afd7e4f6df4d8aa57d12e0212346a2
Meghan Denny <hello@nektro.net>
2025-02-15 22:04:56 -0800

    remove run step

--- build.zig
+++ build.zig
@@ -6,26 +6,6 @@ pub fn build(b: *std.Build) void {
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
-    const exe = b.addExecutable(.{
-        .name = "zig-oauth2",
-        .root_source_file = b.path("main.zig"),
-        .target = target,
-        .optimize = mode,
-    });
-    deps.addAllTo(exe);
-    exe.use_llvm = !disable_llvm;
-    exe.use_lld = !disable_llvm;
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const tests = b.addTest(.{
         .root_source_file = b.path("test.zig"),
         .target = target,
